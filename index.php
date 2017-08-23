e08ae2dc71907589edf76ce2897df76a3b4e8313
Seina Nagai <sna98@canterbury.ac.nz>
2017-08-24 10:19:10 +1200

    add google map

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 $active = "home";
 require_once 'include/header.php';
 ?>
+<div id="map"></div>
 
 <script async defer
-  src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_API_KEY_HERE&callback=initMap">
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDyWgwz17ZqlktEYZ9P8SvdxGDFghGDx8k&callback=initMap">
 </script>
 <script src="scripts/map.js"></script>
 <script>
     $(document).ready(function() {
-
+      }
     });
 </script>
 <?php
