0d0db1a79b2a8716424ad1a8cdf72f36c2aa8345
Seina <31260736+seina1229@users.noreply.github.com>
2017-08-31 10:37:58 +1200

    Update config.php

--- include/config.php
+++ include/config.php
@@ -3,5 +3,5 @@ $hostname = "csse-info263.canterbury.ac.nz";
 $database = "akl_transport";
 $username = "info263";
 $password = "info263";
-$APIKey = ""; # Your API Key here.
+$APIKey = "81fd53734c494d1994bc1236585320c5"; # Your API Key here.
 ?>
