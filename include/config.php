7d0ab37f5f5b0f5b942e0e29e40cc15a25519318
Seina <31260736+seina1229@users.noreply.github.com>
2017-09-18 11:53:06 +1200

    Update config.php
    created a drop down list that allows the user to select a route name

--- include/config.php
+++ include/config.php
@@ -1,16 +1,9 @@
 <?php //config.php
-/*$hostname = "192.168.142.2";
-$database = "akl_transport";
-$username = "DBAdmin";
-$password = "afis233";*/
-
 $hostname = "csse-info263.canterbury.ac.nz";
 $database = "akl_transport";
 $username = "info263";
 $password = "info263";
-
 $APIKey = "81fd53734c494d1994bc1236585320c5"; # Your API Key here.
-
 
 // Create connection
 $conn = new mysqli($hostname, $username, $password, $database);
@@ -18,15 +11,21 @@ $conn = new mysqli($hostname, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+
 $sql = "SELECT * FROM routes";
 $result = $conn->query($sql);
+
+echo '<select name="DROP DOWN ROUTE">'; //opens drop down box
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "route_id: " . $row["route_id"]. " - Name: " . $row["agency_id"]. " " . $row["route_type"]. "<br>";
-    }
+    echo '<option value="'.$row['route_long_name'].'">'.$row['route_long_name'].'</option>';
+}
+        //echo "route_id: " . $row["route_id"]. " - Name: " . $row["agency_id"]. " " . $row["route_type"]. "<br>";
+    
 } else {
     echo "0 results";
 }
+echo '</select>'; //closes the drop doqn box
 $conn->close();
 ?>
