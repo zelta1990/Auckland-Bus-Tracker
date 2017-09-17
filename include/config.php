f52b83858f407116905425914ff11f2b93ec70d8
Seina <31260736+seina1229@users.noreply.github.com>
2017-09-18 11:54:22 +1200

    Update config.php

--- include/config.php
+++ include/config.php
@@ -21,7 +21,7 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
     echo '<option value="'.$row['route_long_name'].'">'.$row['route_long_name'].'</option>';
 }
-        //echo "route_id: " . $row["route_id"]. " - Name: " . $row["agency_id"]. " " . $row["route_type"]. "<br>";
+
     
 } else {
     echo "0 results";
